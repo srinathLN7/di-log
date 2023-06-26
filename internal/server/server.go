d9377f5860bd2635a56db3246c0e11cfb8f81f5b
client <client@client.myguest.virtualbox.org>
2023-06-26 22:22:31 +0200

    init grpc services

---
+++ internal/server/server.go
@@ -0,0 +1,50 @@
+package server
+
+import (
+	"context"
+
+	api "github.com/srinathLN7/proglog/api/v1"
+)
+
+type CommitLog interface {
+	Append(*api.Record) (uint64, error)
+	Read(uint64) (*api.Record, error)
+}
+
+type Config struct {
+	CommitLog CommitLog
+}
+
+type grpcServer struct {
+	api.UnimplementedLogServer
+	*Config
+}
+
+var _ api.LogServer = (*grpcServer)(nil)
+
+func newgrpcServer(config *Config) (srv *grpcServer, err error) {
+	srv = &grpcServer{
+		Config: config,
+	}
+	return srv, nil
+}
+
+// Produce: takes in a `record` and produce the corresponding offset
+func (s *grpcServer) Produce(ctx context.Context, req *api.ProduceRequest) (*api.ProduceResponse, error) {
+	offset, err := s.CommitLog.Append(req.Record)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ProduceResponse{Offset: offset}, nil
+}
+
+// Consume : takes in an offset and return the corresponding record
+func (s *grpcServer) Consume(ctx context.Context, req *api.ConsumeRequest) (*api.ConsumeResponse, error) {
+	record, err := s.CommitLog.Read(req.Offset)
+	if err != nil {
+		return nil, err
+	}
+
+	return &api.ConsumeResponse{Record: record}, nil
+}
