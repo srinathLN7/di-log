f493ad1cdee79c946be838f3bd3f1ca57989be75
client <client@client.myguest.virtualbox.org>
2023-06-29 15:43:49 +0200

    all test cases pass

--- internal/log/segment_test.go
+++ internal/log/segment_test.go
@@ -17,13 +17,11 @@ func TestSegment(t *testing.T) {
 	want := &api.Record{Value: []byte("hello world")}
 
 	c := Config{}
-	c.Segment.MaxIndexBytes = 1024
-	c.Segment.MaxStoreBytes = entWidth * 3
+	c.Segment.MaxStoreBytes = 1024
+	c.Segment.MaxIndexBytes = entWidth * 3
 
-	// setting base offset to 16
 	s, err := newSegment(dir, 16, c)
 	require.NoError(t, err)
-
 	require.Equal(t, uint64(16), s.nextOffset, s.nextOffset)
 	require.False(t, s.IsMaxed())
 
@@ -32,26 +30,23 @@ func TestSegment(t *testing.T) {
 		require.NoError(t, err)
 		require.Equal(t, 16+i, off)
 
-		record, err := s.Read(off)
+		got, err := s.Read(off)
 		require.NoError(t, err)
-		require.Equal(t, want.Value, record.Value)
+		require.Equal(t, want.Value, got.Value)
 	}
+
+	_, err = s.Append(want)
+	require.Equal(t, io.EOF, err)
 
 	// maxed index
 	require.True(t, s.IsMaxed())
 
-	_, err = s.Append(want)
-	//require.NoError(t, err)
-	require.Equal(t, io.EOF, err)
-
 	c.Segment.MaxStoreBytes = uint64(len(want.Value) * 3)
 	c.Segment.MaxIndexBytes = 1024
 
-	// call the segment again with the same base offset and dir and check if segment's is able to load state
-	// from persisted index and log files
 	s, err = newSegment(dir, 16, c)
 	require.NoError(t, err)
-
+	// maxed store
 	require.True(t, s.IsMaxed())
 
 	err = s.Remove()
