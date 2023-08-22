46e65bfb7ac2022a9e27319e01057380d58aaa7f
srinathLN7 <srinath0393@gmail.com>
2023-08-22 11:49:34 +0200

    add distributed-test.go file

---
+++ internal/log/distributed_test.go
@@ -0,0 +1,126 @@
+package log_test
+
+import (
+	"fmt"
+	"net"
+	"os"
+	"reflect"
+	"testing"
+	"time"
+
+	"github.com/hashicorp/raft"
+	api "github.com/srinathLN7/proglog/api/v1"
+	"github.com/srinathLN7/proglog/internal/log"
+	"github.com/stretchr/testify/require"
+	"github.com/travisjeffery/go-dynaport"
+)
+
+func TestMultipleNodes(t *testing.T) {
+
+	var logs []*log.DistributedLog
+	nodeCount := 3
+	ports := dynaport.Get(nodeCount)
+
+	for i := 0; i < nodeCount; i++ {
+
+		// We setup a three-server cluster and shorten the default Raft timeout configs so that Raft elects the leader quickly
+		dataDir, err := os.MkdirTemp("", "distributed-log-test")
+		require.NoError(t, err)
+		defer func(dir string) {
+			_ = os.RemoveAll(dir)
+		}(dataDir)
+
+		ln, err := net.Listen(
+			"tcp",
+			fmt.Sprintf("127.0.0.1:%d", ports[i]),
+		)
+
+		require.NoError(t, err)
+
+		config := log.Config{}
+		config.Raft.StreamLayer = log.NewStreamLayer(ln, nil, nil)
+		config.Raft.LocalID = raft.ServerID(fmt.Sprintf("%d", i))
+		config.Raft.HeartbeatTimeout = 50 * time.Millisecond
+		config.Raft.ElectionTimeout = 50 * time.Millisecond
+		config.Raft.LeaderLeaseTimeout = 50 * time.Millisecond
+		config.Raft.CommitTimeout = 5 * time.Millisecond
+
+		if i == 0 {
+			config.Raft.Bootstrap = true
+		}
+
+		l, err := log.NewDistributedLog(dataDir, config)
+		require.NoError(t, err)
+
+		if i != 0 {
+			// Adds the other two servers to the cluster
+			err = logs[0].Join(
+				fmt.Sprintf("%d", i), ln.Addr().String(),
+			)
+			require.NoError(t, err)
+		} else {
+			// First server bootstraps the cluster and becomes the leader
+			err = l.WaitForLeader(3 * time.Second)
+			require.NoError(t, err)
+		}
+
+		logs = append(logs, l)
+	}
+
+	// Test our replication by appending some records to our leader server `logs[0]`
+	// and check that Raft replicated the records to its followers. Raft followers
+	// will apply the append message after a short latency, so we use testify's `Eventually()`
+	// method to give Raft enough time to finish replicating
+
+	records := []*api.Record{
+		{Value: []byte("first")},
+		{Value: []byte("second")},
+	}
+
+	for _, record := range records {
+		off, err := logs[0].Append(record)
+		require.NoError(t, err)
+
+		require.Eventually(t, func() bool {
+			for j := 0; j < nodeCount; j++ {
+				got, err := logs[j].Read(off)
+				if err != nil {
+					return false
+				}
+
+				if !reflect.DeepEqual(got.Value, record.Value) {
+					return false
+				}
+			}
+			return true
+		}, 500*time.Millisecond, 50*time.Millisecond)
+
+		// Make the first server leave the cluster and test if the leader
+		// stops replicating the logs to the server
+		err = logs[0].Leave("1")
+		require.NoError(t, err)
+
+		time.Sleep(50 * time.Millisecond)
+
+		off, err = logs[0].Append(&api.Record{
+			Value: []byte("third"),
+		})
+
+		require.NoError(t, err)
+
+		time.Sleep(50 * time.Millisecond)
+
+		for j := 1; j < nodeCount; j++ {
+			record, err := logs[j].Read(off)
+			if j == 1 {
+				require.IsType(t, api.ErrOffsetOutOfRange{}, err)
+				require.Nil(t, record)
+			} else {
+				require.NoError(t, err)
+				require.Equal(t, []byte("third"), record.Value)
+				require.Equal(t, off, record.Offset)
+			}
+		}
+	}
+
+}
