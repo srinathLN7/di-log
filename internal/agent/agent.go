4452f8f1012219ad51db7e2116a3854d29eaf3b5
srinathLN7 <srinath0393@gmail.com>
2023-09-05 21:51:04 +0200

    deploy Raft on a fully qualified domain name

--- internal/agent/agent.go
+++ internal/agent/agent.go
@@ -110,8 +110,13 @@ func (a *Agent) setupLogger() error {
 // Raft and grpc connections and then creates the mux with the listener
 // Multiplex (mux) allows yout to serve different services on the same port.
 func (a *Agent) setupMux() error {
+	addr, err := net.ResolveTCPAddr("tcp", a.Config.BindAddr)
+	if err != nil {
+		return err
+	}
 	rpcAddr := fmt.Sprintf(
-		":%d",
+		"%s:%d",
+		addr.IP.String(),
 		a.Config.RPCPort,
 	)
 
@@ -147,11 +152,15 @@ func (a *Agent) setupLog() error {
 		a.Config.ServerTLSConfig,
 		a.Config.PeerTLSConfig,
 	)
+	rpcAddr, err := a.Config.RPCAddr()
+	if err != nil {
+		return err
+	}
 
+	logConfig.Raft.BindAddr = rpcAddr
 	logConfig.Raft.LocalID = raft.ServerID(a.Config.NodeName)
 	logConfig.Raft.Bootstrap = a.Config.Bootstrap
 
-	var err error
 	a.log, err = log.NewDistributedLog(
 		a.Config.DataDir,
 		logConfig,
